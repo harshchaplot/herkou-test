15a6555a8c2da1f6c7ccf347bcd88c653384dbda
unknown <harshchaplot11@gmail.com>
2020-07-09 19:11:52 +0530

    hi

--- app.js
+++ app.js
@@ -77,9 +77,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 // });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
