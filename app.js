fbfb0e866f241cf8a8b7b2a213047e0fed93ee49
Rutvik2901 <gajjarrutvik291@gmail.com>
2020-07-09 16:32:45 +0530

    Some changes

--- app.js
+++ app.js
@@ -91,6 +91,13 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.set( 'port', ( process.env.PORT || 3000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
 
 module.exports = app;
