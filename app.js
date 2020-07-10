99cbf8d28a06d0cf958b7deebf585e4850ff0a38
Rutvik2901 <gajjarrutvik291@gmail.com>
2020-07-10 10:18:03 +0530

    first commit

--- app.js
+++ app.js
@@ -54,10 +54,10 @@ var app = express();
 // view engine setup
 app.use(express.static('dist/quizgenerator'));
 app.get('/*', (req,res) => {
-  res.sendFile(path.join(__dirname,'/dist/quizgenerator/index.html'));
+  // res.sendFile(path.join(__dirname,'/dist/quizgenerator/index.html'));
   console.log(__dirname,process.cwd());
   
-  // res.sendFile(process.cwd()+"/quizgenerator/dist/quizgenerator/index.html");
+  res.sendFile(process.cwd()+"/quizgenerator/dist/quizgenerator/index.html");
 });
 app.listen(process.env.PORT || 3000);
 
