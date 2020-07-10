8f4903bc86eb392821c0a15110cc79db3017b135
Rutvik2901 <gajjarrutvik291@gmail.com>
2020-07-10 16:42:02 +0530

    first commit

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.get('/*', (req,res) => {
   
   res.sendFile(temp+"/quizgenerator/dist/quizgenerator/index.html");
 });
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
