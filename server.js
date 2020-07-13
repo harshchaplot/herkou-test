6950b10a863c2aec26a8ec30422efde9144f6e97
Rutvik2901 <gajjarrutvik291@gmail.com>
2020-07-13 16:15:03 +0530

    first commit

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/quizgenerator'));
+app.use(express.static(__dirname + 'quizgenerator/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+"/quizgenerator/dist/quizgenerator/index.html"));
+res.sendFile(path.join(__dirname+"/quizgenerator/dist/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
